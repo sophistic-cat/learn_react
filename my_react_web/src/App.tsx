8a91ea5fca7f3324b3068784df4abf9b23559903
saber <2373632346@qq.com>
2021-10-05 17:19:16 +0800

    add init file

---
+++ my_react_web/src/App.tsx
@@ -0,0 +1,10 @@
+import UploadButton from './component/uploadButton';
+
+export default function App() {
+  return (
+    <div className="App">
+      
+      <UploadButton />
+    </div>
+  );
+}
---
+++ my_react_web/src/component/uploadButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Excel from 'exceljs';
+
+export default function UploadButton(props:{
+    accept?: string
+}){
+    const upload = (files:any)=>{
+        console.log('uploading...');
+        const filesList = files.target.files;
+        console.log(filesList[0]);
+        
+        const fileReader = new FileReader();
+        fileReader.onload = (ev) =>{
+            const result = ev.target;
+            const test:any = [];
+            console.log(result?.result);
+            const workbook = new Excel.Workbook();
+            // workbook.xlsx.read()
+            workbook.xlsx.load(result?.result??test).then((res)=>{
+                console.log(res);
+                res.eachSheet((_sheet, id)=>{
+                    console.log('_sheet', _sheet)
+                    console.log('id', id);  
+                    _sheet.eachRow((row, rowIndex)=>{
+                        console.log('row.values:', row.values);
+                        console.log('rowIndex:', rowIndex);
+                    })
+                })
+            })
+            
+        }
+
+        fileReader.readAsBinaryString(filesList[0]);
+    }
+    return(
+        <React.Fragment>
+            <input type='file' accept={props.accept ?? '.xlsx, .xls'} onChange={upload} />
+        </React.Fragment>
+    )
+}
