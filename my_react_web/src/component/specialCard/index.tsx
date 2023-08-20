ad1e542a8e51345018a12764669df64aa2aa8ebf
sophistic-cat <2373632346@qq.com>
2023-08-20 21:54:23 +0800

    [PreviewTemplate] optimise version0.2 1. optimise specialCard

--- my_react_web/src/component/specialCard/index.tsx
+++ my_react_web/src/component/specialCard/index.tsx
@@ -1,45 +1,61 @@
-import { Box, Icon } from '@material-ui/core';
+import { Box, makeStyles } from '@material-ui/core';
 import React from 'react';
 
+const useStyle = makeStyles({
+  root: {
+    width: '100%', display: 'flex', flexDirection: 'column'
+  },
+  rightButtonRoot: {
+    display: 'inline-flex', justifyContent: 'space-between', width: '100%'
+  },
+  rightButtonPlaceHolderBox: {
+    width: '100%',
+    height: 'auto',
+    border: '1px solid rgb(205, 210, 219)',
+    borderRadius: '0.375rem 0 0 0',
+    borderWidth: '1px 0 0 1px'
+  },
+  rightButtonBox: {
+    width: '1.6rem',
+    height: '1.5rem',
+    border: '1px solid rgb(205, 210, 219)',
+    borderRadius: '0 0.375rem 0.375rem 0',
+    borderWidth: '1px 1px 1px 0',
+    display: 'inline-flex',
+    justifyContent: 'center'
+  },
+  bodyRoot: {
+    display: 'inline-flex', justifyContent: 'space-between', width: '100%'
+  },
+  bodyBox: {
+    display: 'inline-flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    minHeight: '10rem',
+    border: '1px solid rgb(205, 210, 219)',
+    borderWidth: '0 1px 1px 1px',
+    borderRadius: '0 0 0.375rem 0.375rem'
+  },
+  bodyPlaceHolderBox: {
+    width: '1.6rem', height: 'auto'
+  }
+});
+
 const SpecialCard = React.memo(() => {
-  return <Box style={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
-    <Box style={{ display: 'inline-flex', justifyContent: 'space-between', width: '100%' }}>
-        <Box style={{
-          width: '100%',
-          height: 'auto',
-          border: '1px solid rgb(205, 210, 219)',
-          borderRadius: '0.375rem 0 0 0',
-          borderWidth: '1px 0 0 1px'
-        }}>
-        </Box>
-        <Box style={{
-          width: '1.6rem',
-          height: '1.5rem',
-          border: '1px solid rgb(205, 210, 219)',
-          borderRadius: '0 0.375rem 0.375rem 0',
-          borderWidth: '1px 1px 1px 0'
-        }}>
-            {/* <Icon className='search' /> */}
+  const classes = useStyle();
+
+  return <Box className={classes.root}>
+    <Box className={classes.rightButtonRoot}>
+        <Box className={classes.rightButtonPlaceHolderBox} />
+        <Box className={classes.rightButtonBox}>
             <label>{'×'}</label>
         </Box>
     </Box>
-    <Box style={{ display: 'inline-flex', justifyContent: 'space-between', width: '100%' }}>
-        <Box style={{
-          display: 'inline-flex',
-          justifyContent: 'space-between',
-          width: '100%',
-          minHeight: '10rem',
-          border: '1px solid rgb(205, 210, 219)',
-          borderWidth: '0 1px 1px 1px',
-          borderRadius: '0 0 0.375rem 0.375rem'
-        }}>
+    <Box className={classes.bodyRoot}>
+        <Box className={classes.bodyBox}>
 
         </Box>
-        <Box style={{
-          width: '1.6rem', height: 'auto'
-        }}>
-
-        </Box>
+        <Box className={classes.bodyPlaceHolderBox} />
     </Box>
 </Box>;
 });
